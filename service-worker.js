af84aa0c2f870c27765660e367a48aaf4c7c8e21
EADRYC <eadrycerousmane@gmail.com>
2025-06-03 17:01:58 +0300

    Add service worker for PWA

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'myduka-cache-v1';
+const urlsToCache = [
+  '/',
+  '/?source=pwa',
+  'https://www.mydukaonline.com',
+  'https://i.imgur.com/xydqiZz.png',
+  'https://i.imgur.com/rLigkw2.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
