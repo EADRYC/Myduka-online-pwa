22626b187d79b5e888443af456553c1337e5cb34
EADRYC <eadrycerousmane@gmail.com>
2025-06-03 20:44:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,24 @@
 const CACHE_NAME = 'myduka-cache-v1';
 const urlsToCache = [
   '/',
-  '/?source=pwa',
-  'https://www.mydukaonline.com',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
   'https://i.imgur.com/xydqiZz.png',
   'https://i.imgur.com/rLigkw2.png'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
+// Activate Service Worker
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -33,6 +29,17 @@ self.addEventListener('activate', (event) => {
           }
         })
       );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached file or fetch from network
+      return response || fetch(event.request);
     })
   );
 });
