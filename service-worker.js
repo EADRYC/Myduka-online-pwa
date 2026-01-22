979786d03cf0bdb8fdb56cf6d2c4e25d5fa1e76d
EADRYC <eadrycerousmane@gmail.com>
2026-01-22 14:09:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,14 @@
-const CACHE_NAME = 'myduka-cache-v1';
+const CACHE_NAME = 'e-online-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  'https://i.imgur.com/xydqiZz.png',
-  'https://i.imgur.com/rLigkw2.png'
+  '/', 
+  'https://i.imgur.com/9mKlU68.png'
 ];
 
 // Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Tunatumia {cache: 'reload'} kuhakikisha anapata faili jipya
       return cache.addAll(urlsToCache);
     })
   );
@@ -34,12 +31,12 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// Fetch Requests
+// Fetch Requests - Mkakati wa "Network First"
+// Hii ni muhimu kwa duka ili mteja akibadilisha bei au bidhaa ionekane fasta
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cached file or fetch from network
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
